c45aaf548bbe217b23b98b58c3ea9bde8f22d123
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2024-05-09 19:36:15 +0200

    Add exercises

--- Makefile
+++ Makefile
@@ -1,7 +1,16 @@
-all: ex101.o
+all: ex101.o ex102.o ex103.o ex104.o
 
 ex101.o: ex101.c
 	cc -ansi ex101.c -o ex101.o
+
+ex102.o: ex102.c
+	cc -ansi ex102.c -o ex102.o
+
+ex103.o: ex103.c
+	cc -ansi ex103.c -o ex103.o
+
+ex104.o: ex104.c
+	cc -ansi ex104.c -o ex104.o
 
 clean:
 	rm -rvf *.o
---
+++ ex104.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* print Celsius-Fahrenheit table
+for celsius = -20, 0, 20, ..., 160; floating-point version */
+main()
+{
+	float fahr, celsius;
+	float lower, upper, step;
+	lower = -20; /* lower limit of temperatuire scale */
+	upper = 160; /* upper limit */
+	step = 20; /* step size */
+	celsius = lower;
+	printf("Celsius ");
+	printf("Fahrenheit\n");
+	while (celsius <= upper) {
+		fahr = (9.0/5.0) * celsius + 32.0;
+		printf("%7.0f %10.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
