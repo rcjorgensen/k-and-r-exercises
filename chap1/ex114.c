6e13865377ba529d075aff40319aa065247c67d9
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2024-05-20 10:59:02 +0200

    Add Exercise 1-14

---
+++ chap1/ex114.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define NCHARS	128
+
+/* prints a histogram of the frequencies of different characters in the input */
+main()
+{
+	int c, i;
+	int chars[NCHARS];
+	
+	while ((c = getchar()) != EOF) {
+		++chars[c];
+	}
+
+	for (c = 32; c < NCHARS; ++c) {
+		if (chars[c] > 0) {
+			printf("%c:\t", c);
+			for (i = 0; i < chars[c]; ++i) {
+				printf("#");
+			}
+			printf("\n");
+		}
+	}
+}
